544700dfd9c14c322b65718ccb870da9aaa301e4
dathoangd <hoang.dat.nd93@gmail.com>
2021-06-04 16:34:43 +0700

    update hw12.ex3

--- src/test/java/hw/day12/AssertMaven.java
+++ src/test/java/hw/day12/AssertMaven.java
@@ -3,7 +3,6 @@ import static org.assertj.core.api.Java6Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.apache.commons.validator.routines.EmailValidator;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class AssertMaven {
@@ -17,10 +16,25 @@ public class AssertMaven {
     }
 
     @Test
-    public void testMethod_Weight() {
+    public void testWeight() {
         double weight = 55.5;
         assertThat(weight).isNotNull()
                 .isGreaterThan(0);
     }
+
+    @Test
+    public void testIdentityNumber(){
+       String[] identityNo = {"000909093", "934819804", "1900109332484", "D9933399999", "091122234" };
+        for (String s : identityNo) {
+            try {
+                assertThat(s.length()).isIn(9, 12);
+                assertThat(s).containsOnlyDigits();
+            } catch (AssertionError e) {
+                throw new AssertionError("Identity number is not correct: " + s + " " + e.getMessage());
+            }
+
+        }
+    }
+
 
 }
